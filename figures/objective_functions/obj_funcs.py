33b102a5ab7f67304e7ccacc6684ffb0f6066d9f
ChristianMichelsen <christianmichelsen@gmail.com>
2019-12-17 22:03:15 +0100

    Add own corrections to machine learning theory

--- figures/objective_functions/obj_funcs.py
+++ figures/objective_functions/obj_funcs.py
@@ -7,47 +7,6 @@ plt.style.use("/Volumes/hep/QuarksVsGluons/matplotlibrc")  # bmh
 
 save_plots = True
 
-def arrowed_spines(fig, ax):
-
-    xmin, xmax = ax.get_xlim() 
-    ymin, ymax = ax.get_ylim()
-
-    # removing the default axis on all sides:
-    for side in ['bottom','right','top','left']:
-        ax.spines[side].set_visible(False)
-
-    # removing the axis ticks
-    plt.xticks([]) # labels 
-    plt.yticks([])
-    ax.xaxis.set_ticks_position('none') # tick markers
-    ax.yaxis.set_ticks_position('none')
-
-    # get width and height of axes object to compute 
-    # matching arrowhead length and width
-    dps = fig.dpi_scale_trans.inverted()
-    bbox = ax.get_window_extent().transformed(dps)
-    width, height = bbox.width, bbox.height
-
-    # manual arrowhead width and length
-    hw = 1./20.*(ymax-ymin) 
-    hl = 1./20.*(xmax-xmin)
-    lw = 1. # axis line width
-    ohg = 0.3 # arrow overhang
-
-    # compute matching arrowhead length and width
-    yhw = hw/(ymax-ymin)*(xmax-xmin)* height/width 
-    yhl = hl/(xmax-xmin)*(ymax-ymin)* width/height
-
-    # draw x and y axis
-    ax.arrow(xmin, 0, xmax-xmin, 0., fc='k', ec='k', lw = lw, 
-             head_width=hw, head_length=hl, overhang = ohg, 
-             length_includes_head= True, clip_on = False) 
-
-    ax.arrow(0, ymin, 0., ymax-ymin, fc='k', ec='k', lw = lw, 
-             head_width=yhw, head_length=yhl, overhang = ohg, 
-             length_includes_head= True, clip_on = False)
-
-
 def ae(z):
     return np.sqrt(np.square(z))
 
@@ -84,7 +43,10 @@ x = np.linspace(0, 7, 1000)
 for name, loss in losses.items():
     ax.plot(x, loss(x), '-', label=name, lw=3)
 ax.legend()
-ax.set(xlabel=r'$y-\hat{y}$', ylabel=r'$\ell(y, \hat{y})$', ylim=(0, 8))
+ax.set(ylim=(0, 8))
+ax.set_xlabel(r'$y-\hat{y}$', fontsize=30)
+ax.set_ylabel(r'$\ell(y, \hat{y})$', fontsize=30)
+
 
 # arrowed_spines(fig, ax)
 
@@ -97,7 +59,7 @@ ax.set_position([box.x0,
 
 # Put a legend below current axis
 ax.legend(loc='lower center', bbox_to_anchor=(0.5, 1.05),
-        fancybox=False, shadow=False, ncol=3, frameon=False)
+        fancybox=False, shadow=False, ncol=3, frameon=False, fontsize=24)
 fig.tight_layout()
 
 
@@ -109,7 +71,12 @@ x = np.linspace(0, 2, 1000)
 
 for name, loss in losses.items():
     ax2.plot(x, loss(x), '-', label=name, lw=4)
-ax2.set(xlabel=r'$y-\hat{y}$', ylabel=r'$\ell(y, \hat{y})$', ylim=(0, 1.5))
+# ax2.set(xlabel=r'$y-\hat{y}$', ylabel=r'$\ell(y, \hat{y})$', ylim=(0, 1.5))
+ax2.set(ylim=(0, 1.5))
+ax2.set_xlabel(r'$y-\hat{y}$', fontsize=30)
+ax2.set_ylabel(r'$\ell(y, \hat{y})$', fontsize=30)
+
+
 
 box = ax2.get_position()
 ax2.set_position([box.x0, 
@@ -119,7 +86,7 @@ ax2.set_position([box.x0,
 
 # Put a legend below current axis
 ax2.legend(loc='lower center', bbox_to_anchor=(0.5, 1.05),
-        fancybox=False, shadow=False, ncol=3, frameon=False)
+        fancybox=False, shadow=False, ncol=2, frameon=False, fontsize=20)
 fig2.tight_layout()
 
 
--- figures/ridge_regression/ridge.py
+++ figures/ridge_regression/ridge.py
@@ -7,47 +7,6 @@ plt.style.use("/Volumes/hep/QuarksVsGluons/matplotlibrc")  # bmh
 
 save_plots = True
 
-def arrowed_spines(fig, ax):
-
-    xmin, xmax = ax.get_xlim() 
-    ymin, ymax = ax.get_ylim()
-
-    # removing the default axis on all sides:
-    for side in ['bottom','right','top','left']:
-        ax.spines[side].set_visible(False)
-
-    # removing the axis ticks
-    plt.xticks([]) # labels 
-    plt.yticks([])
-    ax.xaxis.set_ticks_position('none') # tick markers
-    ax.yaxis.set_ticks_position('none')
-
-    # get width and height of axes object to compute 
-    # matching arrowhead length and width
-    dps = fig.dpi_scale_trans.inverted()
-    bbox = ax.get_window_extent().transformed(dps)
-    width, height = bbox.width, bbox.height
-
-    # manual arrowhead width and length
-    hw = 1./20.*(ymax-ymin) 
-    hl = 1./20.*(xmax-xmin)
-    lw = 1. # axis line width
-    ohg = 0.3 # arrow overhang
-
-    # compute matching arrowhead length and width
-    yhw = hw/(ymax-ymin)*(xmax-xmin)* height/width 
-    yhl = hl/(xmax-xmin)*(ymax-ymin)* width/height
-
-    # draw x and y axis
-    ax.arrow(xmin, 0, xmax-xmin, 0., fc='k', ec='k', lw = lw, 
-             head_width=hw, head_length=hl, overhang = ohg, 
-             length_includes_head= True, clip_on = False) 
-
-    ax.arrow(0, ymin, 0., ymax-ymin, fc='k', ec='k', lw = lw, 
-             head_width=yhw, head_length=yhl, overhang = ohg, 
-             length_includes_head= True, clip_on = False)
-
-
 from sklearn.datasets import make_regression
 from sklearn.linear_model import Ridge
 from iminuit import Minuit
@@ -118,9 +77,11 @@ for lamb in [0, 0.001, 0.1, 10]:
 
     ax.plot(xx, y_fit, '-', lw=2, label=s) #r'$\lambda = $'+f'{lamb:.4f}')
 
-ax.set(xlabel=r'$x$', ylabel=r'$f(x)$', 
+# ax.set(xlabel=, ylabel=, 
     #    ylim=(-20, 50),
-       )
+    #    )
+ax.set_xlabel(r'$x$', fontsize=32)
+ax.set_ylabel(r'$f(x)$', fontsize=32)
 ax.legend()
 
 box = ax.get_position()
@@ -131,9 +92,9 @@ ax.set_position([box.x0,
         box.height * 0.9])
 
 # Put a legend below current axis
-ax.legend(loc='lower center', bbox_to_anchor=(0.45, 1.01),
+ax.legend(loc='lower center', bbox_to_anchor=(0.4, 1.01),
         fancybox=False, shadow=False, ncol=1, frameon=False,
-        fontsize=20)
+        fontsize=28)
 
 
 # fig
