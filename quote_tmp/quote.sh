929ba815a429e01914d5f4520c0153f5f56366f6
ChristianMichelsen <christianmichelsen@gmail.com>
2019-12-29 08:53:25 +0100

    Rewrite the Part 1+2 and add new intro to q-g sec

--- quote_tmp/quote.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# new fortunes every hour
-if [ ! -f ./tmp/fortune-$1.tex ] || test "`find ./tmp/fortune-$1.tex -mmin +60`"
-then
-    fortune | sed -e 's/-- .*//g' | sed '/^\s*$/d' > ./tmp/fortune-$1.tex
-fi
